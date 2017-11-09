ad38ae59716a17ed492abe4a58442d4c68a71e3a
Leonardo Lobeto <llobeto@despegar.com>
2017-11-09 18:19:04 -0300

    sgsgs

--- nightwatch/tests/test-base.js
+++ nightwatch/tests/test-base.js
@@ -1,11 +1,6 @@
 this.failedTest = function (browser) {
   browser
     .url('http://www.google.com')
-    .waitForElementVisible('body', 1000)
-    .setValue('input[type=text]', 'nightwatch')
-    .waitForElementVisible('button[name=btnG]', 1000)
-    .click('button[name=btnG]')
-    .pause(1000)
     .assert.containsText('#main', 'Sorete The Night Watch')
     .end();
 };
@@ -15,13 +10,5 @@ this.errorTest = function (browser) {
 };
 
 this.okTest = function (browser) {
-  browser
-    .url('http://www.google.com')
-    .waitForElementVisible('body', 1000)
-    .setValue('input[type=text]', 'nightwatch')
-    .waitForElementVisible('button[name=btnG]', 1000)
-    .click('button[name=btnG]')
-    .pause(2000)
-    .assert.containsText('#main', 'The Night Watch')
-    .end();
+  browser.end();
 };
